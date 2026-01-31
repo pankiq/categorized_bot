0e166d23a4772e0d14da4226bbc8e7f979f5d4f7
pankiq <90942597+pankiq@users.noreply.github.com>
2026-02-01 00:09:40 +1000

    Update state.py

--- state.py
+++ state.py
@@ -1,1 +1,22 @@
+from datetime import datetime
 
+# Глобальные переменные состояния
+PENDING_VERIFICATION = {}
+PROCESSED_ALBUMS = []
+LAST_MESSAGE_TIME = datetime.now()
+AI_COOLDOWN_TIME = datetime.now()
+SUMMARY_COOLDOWN_TIME = datetime.now()
+
+# Турнир
+TOURNAMENT_ACTIVE = False
+TOURNAMENT_MAX_PLAYERS = 0
+TOURNAMENT_PLAYERS = []
+TOURNAMENT_USERNAMES = []
+
+# Чат и Лор
+CHAT_HISTORY = []
+SILENT_MODE_USERS = []
+USED_LORE_FACTS = []
+
+# Игры
+ACTIVE_DUELS = {} # Загрузим в database.py или games.py
